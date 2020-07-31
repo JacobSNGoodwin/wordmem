90b1274372bc64be4641ee964bf504bbc5e92bfc
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-07-31 14:11:53 -0700

    Use vue composition api to set isLogin state

--- auth/login-form/src/App.vue
+++ auth/login-form/src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="section">
+      <Auth />
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Auth from "./components/Auth.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Auth,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ auth/login-form/src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2 class="title is-3 has-text-centered">{{ isLogin ? "Login" : "Sign Up" }}</h2>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  props: {
+    isLogin: Boolean,
+  },
+};
+</script>

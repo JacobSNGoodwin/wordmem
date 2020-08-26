ac6161de3064fecc667d9c83da535c1cb1463f63
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-08-26 10:03:59 -0700

    Install vue-router

--- account-client/src/main.js
+++ account-client/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./scss/index.scss";
+// import router from './router'
 
+// createApp(App).use(router).mount("#app");
 createApp(App).mount("#app");

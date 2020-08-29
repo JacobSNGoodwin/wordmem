b60120f0755ee9a3ba2d8013f95422bce92b5271
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-08-28 18:45:41 -0700

    Use axios instead of fetch

--- account-client/src/store/auth.js
+++ account-client/src/store/auth.js
@@ -1,4 +1,6 @@
 import { reactive, provide, inject, toRefs } from "@vue/composition-api";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
 
 const state = reactive({
   currentUser: null,
@@ -46,32 +48,29 @@ export function useAuth() {
 
 // authenticate implements common code between signin and signup
 const authenticate = async (email, password, endpoint) => {
-  const req = new Request(endpoint, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      email,
-      password
-    })
-  });
-
   state.isLoading = true;
 
   try {
-    const res = await fetch(req);
-    const { user, tokens } = await res.json();
+    const res = await axios.post(endpoint, { email, password });
+
+    const { tokens } = res.data;
 
     state.idToken = tokens.idToken;
     state.refreshToken = tokens.refreshToken;
-    state.currentUser = user;
+
+    const tokenClaims = jwt_decode(tokens.idToken);
+
+    // set tokens to local storage with expiry (separate function)
+    state.currentUser = tokenClaims.user;
   } catch (e) {
-    console.log(e);
+    // e.response for non 200
+    // e.request for requet errors
+    // else some other error
+    console.log(e.response);
     state.currentUser = null;
     state.idToken = null;
     state.refreshToken = null;
-    state.error = new Error("Error fetching user");
+    state.error = e;
   } finally {
     state.isLoading = false;
   }

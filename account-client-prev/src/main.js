0dbbb4f591edbcde6ba3e237a99bbf167dee448b
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-09-07 16:32:38 -0700

    Remove prev vue app

--- account-client-prev/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-
-import "./scss/index.scss";
-// import router from './router'
-
-// createApp(App).use(router).mount("#app");
-createApp(App).mount("#app");

482e99fc34cc7705231266da136f1e9278b4d463
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-09-11 13:47:38 -0700

    Add SQL models for words

---
+++ words/src/migrations/00001_initial.down.sql
@@ -0,0 +1,2 @@
+DROP TABLE words;
+DROP EXTENSION IF EXISTS "uuid-ossp";
---
+++ words/src/migrations/00001_initial.up.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS words (
+  id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  userId uuid NOT NULL,
+  email VARCHAR NOT NULL,
+  word VARCHAR NOT NULL,
+  ref_url VARCHAR NOT NULL DEFAULT '',
+  email_reminder BOOLEAN DEFAULT FALSE,
+  start_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);

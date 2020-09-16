ad5e8d16b2b4d2ea69ba1b8483362a57b4e3a323
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-09-16 09:38:17 -0700

    Install bulma

--- word-client/src/App.tsx
+++ word-client/src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Hello World</h1>
+        <p className="subtitle">
+          My first website with <strong>Bulma</strong>!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </section>
   );
 }
 
